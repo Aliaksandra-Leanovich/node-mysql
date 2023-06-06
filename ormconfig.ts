1c95172fdd5a3e98b0f7b9db5d95070a3d53f520
aliaksandraleanovich <a.lenovich@softteco.com>
2023-06-06 10:51:14 +0300

    feat: auth model

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,16 @@
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectionSource = new DataSource({
   migrationsTableName: "migrations",
   type: "mysql",
   host: "localhost",
-  port: 3307,
-  username: "test",
-  password: "testpass",
-  database: "testdb",
+  port: Number(process.env.DB_PORT) || 3307,
+  username: process.env.DB_USER || "user",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_DATABASE || "db",
   logging: false,
   synchronize: false,
   name: "default",
