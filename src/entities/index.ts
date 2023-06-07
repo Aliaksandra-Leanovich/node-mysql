228081ab38614604439e55cb62fbfd3be4fe46e9
aliaksandraleanovich <a.lenovich@softteco.com>
2023-06-07 15:41:16 +0300

    feat: add export

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -4,3 +4,4 @@ export { TestCandidate } from "./TestCandidates";
 export { Test } from "./Test";
 export { Questions } from "./Question";
 export { TestQuestions } from "./TestQuestion";
+export { Answers } from "./Answer";
