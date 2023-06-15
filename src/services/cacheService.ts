132edd759a1bf73ccea5a42e8955d478e482f560
aliaksandraleanovich <a.lenovich@softteco.com>
2023-06-15 10:37:55 +0300

    fix: fix docker

---
+++ src/services/cacheService.ts
@@ -0,0 +1,13 @@
+const redis = require("ioredis");
+
+const cache = (req, res, next) => {
+  const { id } = req.params;
+  redis.get(id, (error, result) => {
+    if (error) throw error;
+    if (result !== null) {
+      return res.json(JSON.parse(result));
+    } else {
+      return next();
+    }
+  });
+};
